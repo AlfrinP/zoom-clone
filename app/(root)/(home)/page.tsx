b0c13c62a40cbeba20ec380e542ff196ffbdf776
Mick Suraj <micksuraj999@gmail.com>
2024-05-16 20:03:14 +0530

    fix the error of time

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,17 +1,28 @@
+'use client'
+import { useState, useEffect } from 'react';
 import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
-  const now = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }));
+  const [time, setTime] = useState('');
+  const [date, setDate] = useState('');
 
-  const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now)
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date(new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }));
+      const currentTime = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
+      const currentDate = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
+      setTime(currentTime);
+      setDate(currentDate);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
+            Upcoming Meeting at:  Upcoming
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl uppercase">{time}</h1>
@@ -19,8 +30,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-
-      <MeetingTypeList />
+      <MeetingTypeList/>
     </section>
   );
 };
